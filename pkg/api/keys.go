a63655a24a1ef45602a93d531d63947bcaf3d6e9
rainbowemoji <devzone210@gmail.com>
2022-03-11 01:19:14 +0900

    implement create & get bet history data

--- pkg/api/keys.go
+++ pkg/api/keys.go
@@ -18,7 +18,6 @@ func (c *Context) PingStore(w http.ResponseWriter, r *http.Request, ps httproute
 
 	//i, err := redis.String(conn.Do("PING"))
 	pong, err := c.rs.Client.Ping().Result()
-	//Set(ctx, "key", "value", 0).Err()
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
@@ -34,23 +33,8 @@ func (c *Context) PingStore(w http.ResponseWriter, r *http.Request, ps httproute
 }
 
 func (c *Context) GetKey(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
-	// ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	// defer cancel()
-
-	key := ps.ByName("key")
-
-	//conn, err := c.rs.Pool.GetContext(ctx)
-	//defer conn.Close()
-
-	// if err != nil {
-	// 	w.Header().Set("Content-Type", "application/json")
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	c._log.Error("getting connection", err)
-	// 	return
-	// }
-
-	//i, err := redis.String(conn.Do("GET", key))
+	key := ps.ByName("id")
+	c._log.Info("getting key", key)
 	result, err := c.rs.Client.Get(key).Result()
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
@@ -59,32 +43,28 @@ func (c *Context) GetKey(w http.ResponseWriter, r *http.Request, ps httprouter.P
 		return
 	}
 
+	var output map[string]interface{}
+
+	json.Unmarshal([]byte(result), &output)
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	byt, _ := json.Marshal(output)
+	w.Write(byt)
 
-	bytes, _ := json.Marshal(result)
-	w.Write(bytes)
 }
 
 func (c *Context) CreateKey(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	key := ps.ByName("id")
+	var val map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&val)
+	if err != nil {
+		c._log.Error("creating key", key, err)
+	}
 
-	// ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	// defer cancel()
+	byt, _ := json.Marshal(val)
 
-	key := ps.ByName("key")
-	value := ps.ByName("value")
-	// conn, err := c.rs.Pool.GetContext(ctx)
-	// defer conn.Close()
-
-	// if err != nil {
-	// 	w.Header().Set("Content-Type", "application/json")
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	c._log.Error("getting connection", err)
-	// 	return
-	// }
-
-	// i, err := redis.String(conn.Do("SET", key, value))
-	result, err := c.rs.Client.Set(key, value, 36000*time.Hour).Result()
+	result, err := c.rs.Client.Set(key, string(byt), 36000*time.Hour).Result()
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
@@ -100,23 +80,8 @@ func (c *Context) CreateKey(w http.ResponseWriter, r *http.Request, ps httproute
 }
 
 func (c *Context) DeleteKey(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
-	// ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	// defer cancel()
-
-	key := ps.ByName("key")
-	// conn, err := c.rs.Pool.GetContext(ctx)
-	// defer conn.Close()
-
-	// if err != nil {
-	// 	w.Header().Set("Content-Type", "application/json")
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	c._log.Error("getting connection", err)
-	// 	return
-	// }
-
-	//i, err := redis.String(conn.Do("DEL", key))
-	result, err := c.rs.Client.Del().Result()
+	key := ps.ByName("id")
+	result, err := c.rs.Client.Del(key).Result()
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
